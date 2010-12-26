d2dd2c4b34188c2fe7bb8d652d92c9ad9284b6d1
semicircle21 <semicircle21@gmail.com>
2010-12-26 09:23:53 +0800

    first commit.

---
+++ src/net/learntofly321/TouchGraffiti2SNS/TouchGraffiti2SNS.java
@@ -0,0 +1,13 @@
+package net.learntofly321.TouchGraffiti2SNS;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TouchGraffiti2SNS extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
